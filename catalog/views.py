7a7bc1a63f244f32a0463ac0d0b63aa3288218ef
Nikita Konoplin <franklin.d.kitamory@gmail.com>
2015-01-29 18:18:02 +0700

    Собираем дерево категорий

--- catalog/views.py
+++ catalog/views.py
@@ -15,7 +15,17 @@ def index(request):
 
 def categories(reguest, category_name):
     categories = ItemCategory.objects.all()
-    print categories.slag.filter(parent__isnull=True)
+    indexCategories = []
+    for category in categories:
+        if category.parent is None:
+            indexCategories.append(category)
+            childCategories = []
+            for child in category.child.all():
+                childCategories.append(child)
+
+            indexCategories.append(childCategories)
+
+    print indexCategories
     # items = categories.Item.objects.all()
     tpl = loader.get_template('categories.html')
     c = Context({'categories': categories})
