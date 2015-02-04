b050bb74636deb407334af41d4e5d5f0bc0081e0
Nikita Konoplin <franklin.d.kitamory@gmail.com>
2015-02-04 18:41:47 +0700

    Почистил urls.py от всякого левого

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,17 +5,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('mycatalog.catalog.views',
     url(r'^$', 'index', name='index'),
     url(r'^$', 'categories'),
-    # url(r'(?P<category>\w)/$', 'categories'),
-    # url(r'(?P<category>[A-Za-z0-9_-]+)[/]$', 'categories'),
-
-    # url(r'([a-zA-Z0-9_-]+)/$', 'categories'), Рабочее
-
     url(r'^(?P<category>((?:[\w-]+/){1,3}))$', 'categories'),
-
-
-    # url(r'^([A-Za-z0-9_-]+/)|+$', 'categories'),
-
-    url(r'^category/$', 'categories'),
+    # url(r'^category/$', 'categories'),
 )
-
-# /^[a-z0-9-]+$/
